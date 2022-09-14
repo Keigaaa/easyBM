3894d94f8da48a8347e67982a31da31d47cc0828
Keigaaa <franck.auzuech@hotmail.fr>
2022-09-14 11:09:36 +0200

    Created folder structure
    - model
    - controller
    - ressource
    - factory
    - seeder

---
+++ app/Http/Resources/BookmarkResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookmarkResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'url' => $this->url,
+            'commentary' => $this->commentary,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y'),
+        ];
+    }
+}
